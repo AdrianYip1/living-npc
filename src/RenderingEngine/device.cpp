d9849d27a5221fa6adb1a110a882377ede395129
Adrian Yip <yipadrian1119@gmail.com>
2026-09-19 01:27:41 -0400

    feat: added the command pool, swapchain, and depth/color resources using the helper functions created earler. Re-enabled the helpers in cmake so everything compiles

--- src/RenderingEngine/device.cpp
+++ src/RenderingEngine/device.cpp
@@ -1,4 +1,5 @@
 #include "device.hpp"
+#include "Helpers/image.hpp"
 
 // Debug callback for validation
 static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
@@ -37,9 +38,27 @@ HTN::Device::Device(Window& _window) : window(_window) {
 	createSurface();
 	pickPhysicalDevice();
 	createLogicalDevice();
+	createCommandPool();
+	createSwapchain();
+	createColorResources();
+	createDepthResources();
 }
 
 HTN::Device::~Device() {
+	vkDestroyImageView(device, depthImageView, nullptr);
+	vkDestroyImage(device, depthImage, nullptr);
+	vkFreeMemory(device, depthImageMemory, nullptr);
+
+	vkDestroyImageView(device, colorImageView, nullptr);
+	vkDestroyImage(device, colorImage, nullptr);
+	vkFreeMemory(device, colorImageMemory, nullptr);
+
+	for (auto imageView : swapchainImageViews) {
+		vkDestroyImageView(device, imageView, nullptr);
+	}
+
+	vkDestroySwapchainKHR(device, swapchain, nullptr);
+	vkDestroyCommandPool(getDevice(), commandPool, nullptr);
 	vkDestroyDevice(device, nullptr);
 
 	if (HTN_DEBUG) {
@@ -87,7 +106,7 @@ void HTN::Device::createInstance() {
 	}
 
 	if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
-		throw std::runtime_error("Failed to create instance");
+		throw std::runtime_error("ERROR: Failed to create instance");
 	}
 }
 
@@ -148,13 +167,13 @@ void HTN::Device::setupDebugMessenger() {
 	populateDebugMessengerCreateInfo(createInfo);
 
 	if (CreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS) {
-		throw std::runtime_error("Failed to setup debug messenger");
+		throw std::runtime_error("ERROR: Failed to setup debug messenger");
 	}
 }
 
 void HTN::Device::createSurface() {
 	if (glfwCreateWindowSurface(instance, window.getWindow(), nullptr, &surface) != VK_SUCCESS) {
-		throw std::runtime_error("Failed to create window surface");
+		throw std::runtime_error("ERROR: Failed to create window surface");
 	}
 }
 
@@ -163,7 +182,7 @@ void HTN::Device::pickPhysicalDevice() {
 	vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
 
 	if (deviceCount == 0) {
-		throw std::runtime_error("Failed to find GPUs with Vulkan support");
+		throw std::runtime_error("ERROR: Failed to find GPUs with Vulkan support");
 	}
 
 	std::vector<VkPhysicalDevice> devices(deviceCount);
@@ -177,7 +196,7 @@ void HTN::Device::pickPhysicalDevice() {
 	}
 
 	if (physicalDevice == VK_NULL_HANDLE) {
-		throw std::runtime_error("Failed to find suitable GPU");
+		throw std::runtime_error("ERROR: Failed to find suitable GPU");
 	}
 	msaaSamples = getMaxUsableSampleCount();
 }
@@ -269,7 +288,7 @@ void HTN::Device::createLogicalDevice() {
 	createInfo.ppEnabledExtensionNames = deviceExtensions.data();
 
 	if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS) {
-		throw std::runtime_error("Failed to create logical device");
+		throw std::runtime_error("ERROR: Failed to create logical device");
 	}
 
 	vkGetDeviceQueue(device, indices.graphicsAndComputeFamily.value(), 0, &graphicsQueue);
@@ -329,4 +348,188 @@ HTN::SwapChainSupportDetails HTN::Device::querySwapChainSupport(VkPhysicalDevice
 	}
 
 	return details;
+}
+
+void HTN::Device::createCommandPool() {
+	QueueFamilyIndices queueFamilyIndices = findQueueFamilies(getPhysicalDevice());
+
+	VkCommandPoolCreateInfo poolInfo{};
+	poolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+	poolInfo.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+	poolInfo.queueFamilyIndex = queueFamilyIndices.graphicsAndComputeFamily.value();
+
+	if (vkCreateCommandPool(getDevice(), &poolInfo, nullptr, &commandPool) != VK_SUCCESS) {
+		throw std::runtime_error("ERROR: Failed to create command pool");
+	}
+}
+
+VkSurfaceFormatKHR HTN::Device::chooseSwapchainSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& formats) {
+	for (const auto& format : formats) {
+		if (format.format == VK_FORMAT_B8G8R8A8_SRGB &&
+			format.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
+			return format;
+		}
+	}
+	return formats[0];
+}
+
+VkPresentModeKHR HTN::Device::chooseSwapchainPresentMode(const std::vector<VkPresentModeKHR>& presentModes) {
+	for (const auto& mode : presentModes) {
+		if (mode == VK_PRESENT_MODE_MAILBOX_KHR) {
+			return mode;
+		}
+	}
+	return VK_PRESENT_MODE_FIFO_KHR;
+}
+
+VkExtent2D HTN::Device::chooseSwapchainExtent(const VkSurfaceCapabilitiesKHR& capabilities) {
+	if (capabilities.currentExtent.width != std::numeric_limits<u32>::max()) {
+		return capabilities.currentExtent;
+	}
+	else {
+		int w, h;
+		glfwGetFramebufferSize(window.getWindow(), &w, &h);
+
+		VkExtent2D actualExtent = { static_cast<u32>(w), static_cast<u32>(h) };
+
+		actualExtent.width = std::clamp(actualExtent.width, capabilities.minImageExtent.width, capabilities.maxImageExtent.width);
+		actualExtent.height = std::clamp(actualExtent.height, capabilities.minImageExtent.height, capabilities.maxImageExtent.height);
+
+		return actualExtent;
+	}
+}
+
+void HTN::Device::createSwapchain() {
+	SwapChainSupportDetails swapchainSupport = querySwapChainSupport(physicalDevice);
+
+	VkSurfaceFormatKHR surfaceFormat = chooseSwapchainSurfaceFormat(swapchainSupport.formats);
+	VkPresentModeKHR presentMode = chooseSwapchainPresentMode(swapchainSupport.presentModes);
+	VkExtent2D extent = chooseSwapchainExtent(swapchainSupport.capabilities);
+
+	u32 imageCount = swapchainSupport.capabilities.minImageCount + 1;
+
+	if (swapchainSupport.capabilities.maxImageCount > 0 &&
+		swapchainSupport.capabilities.maxImageCount < imageCount) {
+		imageCount = swapchainSupport.capabilities.maxImageCount;
+	}
+
+	VkSwapchainCreateInfoKHR createInfo{};
+	createInfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+	createInfo.surface = surface;
+	createInfo.minImageCount = imageCount;
+	createInfo.imageFormat = surfaceFormat.format;
+	createInfo.imageColorSpace = surfaceFormat.colorSpace;
+	createInfo.imageExtent = extent;
+	createInfo.imageArrayLayers = 1;
+	createInfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+
+	QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+	u32 queueFamilyIndices[] = { indices.graphicsAndComputeFamily.value(), indices.presentFamily.value() };
+
+	if (indices.graphicsAndComputeFamily != indices.presentFamily) {
+		createInfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+		createInfo.queueFamilyIndexCount = 2;
+		createInfo.pQueueFamilyIndices = queueFamilyIndices;
+	}
+	else {
+		createInfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+		createInfo.queueFamilyIndexCount = 0;
+		createInfo.pQueueFamilyIndices = nullptr;
+	}
+
+	createInfo.preTransform = swapchainSupport.capabilities.currentTransform;
+	createInfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;
+	createInfo.presentMode = presentMode;
+	createInfo.clipped = VK_TRUE;
+	createInfo.oldSwapchain = VK_NULL_HANDLE;
+
+	if (vkCreateSwapchainKHR(device, &createInfo, nullptr, &swapchain) != VK_SUCCESS) {
+		throw std::runtime_error("ERROR: Failed to create swapchain");
+	}
+
+	vkGetSwapchainImagesKHR(device, swapchain, &imageCount, nullptr);
+	swapchainImages.resize(imageCount);
+	vkGetSwapchainImagesKHR(device, swapchain, &imageCount, swapchainImages.data());
+
+	swapchainImageFormat = surfaceFormat.format;
+	swapchainExtent = extent;
+
+	swapchainImageViews.resize(swapchainImages.size());
+	createImageViews();
+}
+
+void HTN::Device::createImageViews() {
+	swapchainImageViews.resize(swapchainImages.size());
+	for (size_t i = 0; i < swapchainImages.size(); i++) {
+		Image::createImageView(device, swapchainImages[i], swapchainImageFormat, VK_IMAGE_ASPECT_COLOR_BIT,
+			swapchainImageViews[i], 1);
+	}
+}
+
+void HTN::Device::recreateSwapchain() {
+	int width = 0, height = 0;
+	glfwGetFramebufferSize(window.getWindow(), &width, &height);
+	while (width == 0 || height == 0) {
+		glfwGetFramebufferSize(window.getWindow(), &width, &height);
+		glfwWaitEvents();
+	}
+
+	vkDeviceWaitIdle(device);
+
+	createSwapchain();
+	createImageViews();
+	createColorResources();
+	createDepthResources();
+}
+
+void HTN::Device::createDepthResources() {
+	VkFormat format = findDepthFormat();
+
+	Image::createImage(*this, swapchainExtent.width, swapchainExtent.height, 1, msaaSamples,
+					   format, VK_IMAGE_TILING_OPTIMAL,
+					   VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT,
+					   VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+					   depthImage, depthImageMemory);
+	Image::createImageView(device, depthImage, format, VK_IMAGE_ASPECT_DEPTH_BIT, depthImageView, 1);
+	Image::transitionImageLayout(*this, depthImage, format, VK_IMAGE_LAYOUT_UNDEFINED,
+								 VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL, 1);
+}
+
+void HTN::Device::createColorResources() {
+	VkFormat format = swapchainImageFormat;
+
+	Image::createImage(*this, swapchainExtent.width, swapchainExtent.height, 1, msaaSamples,
+					   format, VK_IMAGE_TILING_OPTIMAL,
+					   VK_IMAGE_USAGE_TRANSIENT_ATTACHMENT_BIT | VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
+					   VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+					   colorImage, colorImageMemory);
+	Image::createImageView(device, colorImage, format, VK_IMAGE_ASPECT_COLOR_BIT, colorImageView, 1);
+}
+
+VkFormat HTN::Device::findDepthFormat() {
+	return findSupportedFormat(
+		{ VK_FORMAT_D32_SFLOAT, VK_FORMAT_D32_SFLOAT_S8_UINT, VK_FORMAT_D24_UNORM_S8_UINT },
+		VK_IMAGE_TILING_OPTIMAL,
+		VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT
+	);
+}
+
+VkFormat HTN::Device::findSupportedFormat(const std::vector<VkFormat>& candidates,
+										  VkImageTiling tiling, VkFormatFeatureFlags features) {
+	for (VkFormat format : candidates) {
+		VkFormatProperties props;
+		vkGetPhysicalDeviceFormatProperties(physicalDevice, format, &props);
+
+		if (tiling == VK_IMAGE_TILING_LINEAR && (props.linearTilingFeatures & features) == features) {
+			return format;
+		}
+		else if (tiling == VK_IMAGE_TILING_OPTIMAL && (props.optimalTilingFeatures & features) == features) {
+			return format;
+		}
+	}
+	throw std::runtime_error("ERROR: Failed to find supported format");
+}
+
+bool HTN::Device::hasStencilComponent(VkFormat format) {
+	return format == VK_FORMAT_D32_SFLOAT_S8_UINT || format == VK_FORMAT_D24_UNORM_S8_UINT;
 }
--- src/RenderingEngine/device.hpp
+++ src/RenderingEngine/device.hpp
@@ -13,6 +13,8 @@
 #include <set>
 #include <optional>
 #include <string>
+#include <algorithm>
+#include <limits>
 
 namespace HTN {
 
@@ -30,7 +32,7 @@ namespace HTN {
 
 		bool isComplete() {
 			return graphicsAndComputeFamily.has_value() &&
-				presentFamily.has_value();
+				   presentFamily.has_value();
 		}
 	};
 
@@ -57,19 +59,45 @@ namespace HTN {
 
 		SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device);
 		QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
+		VkFormat findSupportedFormat(const std::vector<VkFormat>& candidates, VkImageTiling tiling, VkFormatFeatureFlags features);
+		bool hasStencilComponent(VkFormat format);
+
+		VkFormat findDepthFormat();
+
+		void createSwapchain();
+		void recreateSwapchain();
 
 	private:
 		Window& window;
 
-		// vk objects
+		// instance + surface + validation 
 		VkInstance instance = VK_NULL_HANDLE;
 		VkDebugUtilsMessengerEXT debugMessenger = VK_NULL_HANDLE;
 		VkSurfaceKHR surface = VK_NULL_HANDLE;
+
+		// devices
 		VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
 		VkDevice device = VK_NULL_HANDLE;
 		VkQueue graphicsQueue = VK_NULL_HANDLE;
 		VkQueue presentQueue = VK_NULL_HANDLE;
+		VkCommandPool commandPool;
 		VkSampleCountFlagBits msaaSamples = VK_SAMPLE_COUNT_1_BIT;
+
+		// swapchains
+		VkSwapchainKHR swapchain;
+		std::vector<VkImage> swapchainImages;
+		VkFormat swapchainImageFormat;
+		VkExtent2D swapchainExtent;
+		std::vector<VkImageView> swapchainImageViews;
+
+		// imagges
+		VkImage depthImage;
+		VkDeviceMemory depthImageMemory;
+		VkImageView depthImageView;
+
+		VkImage colorImage;
+		VkDeviceMemory colorImageMemory;
+		VkImageView colorImageView;
 
 		void createInstance();
 		bool checkValidationLayerSupport();
@@ -77,11 +105,22 @@ namespace HTN {
 		void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
 		void setupDebugMessenger();
 		void createSurface();
+
 		void pickPhysicalDevice();
 		bool isDeviceSuitable(VkPhysicalDevice device);
 		bool checkDeviceExtensionSupport(VkPhysicalDevice device);
 		VkSampleCountFlagBits getMaxUsableSampleCount();
 		void createLogicalDevice();
+
+		void createCommandPool();
+
+		VkSurfaceFormatKHR chooseSwapchainSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& formats);
+		VkPresentModeKHR chooseSwapchainPresentMode(const std::vector<VkPresentModeKHR>& presentModes);
+		VkExtent2D chooseSwapchainExtent(const VkSurfaceCapabilitiesKHR& capabilities);
+
+		void createImageViews();
+		void createDepthResources();
+		void createColorResources();
 	};
 
 } // namespace HTN
