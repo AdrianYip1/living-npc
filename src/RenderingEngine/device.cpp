0f421e9f0fa1487e5af69b68fddfbf33e6acb14f
Adrian Yip <yipadrian1119@gmail.com>
2026-09-19 01:05:26 -0400

    feat: validation layers for debugging mode and added the vulkan instance + surface which connects the gltf window with vulkan

--- src/RenderingEngine/device.cpp
+++ src/RenderingEngine/device.cpp
@@ -1,13 +1,155 @@
 #include "device.hpp"
 
-namespace HTN {
+// Debug callback for validation
+static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+	VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+	VkDebugUtilsMessageTypeFlagsEXT messageType,
+	const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+	void* pUserData) {
 
-Device::Device(Window& window) : window(window) {
-	createInstance();
+	std::cerr << "Validation: " << pCallbackData->pMessage << std::endl;
+
+	return VK_FALSE;
 }
 
-Device::~Device() {}
+// Debug messenger for validation
+VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
+	auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+	if (func != nullptr) {
+		return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+	}
+	else {
+		return VK_ERROR_EXTENSION_NOT_PRESENT;
+	}
+}
 
-void Device::createInstance() {}
+// Debug destroy for validation
+void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator) {
+	auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+	if (func != nullptr) {
+		func(instance, debugMessenger, pAllocator);
+	}
+}
 
-} // namespace HTN
+HTN::Device::Device(Window& _window) : window(_window) {
+	createInstance();
+	setupDebugMessenger();
+	createSurface();
+}
+
+HTN::Device::~Device() {
+	if (HTN_DEBUG) {
+		DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
+	}
+
+	vkDestroySurfaceKHR(instance, surface, nullptr);
+	vkDestroyInstance(instance, nullptr);
+}
+
+void HTN::Device::createInstance() {
+	if (HTN_DEBUG && !checkValidationLayerSupport()) {
+		throw std::runtime_error("ERROR: Validation layers not available but requested");
+	}
+
+	constexpr u32 HTN_VERSION = VK_MAKE_VERSION(0, 0, 1);
+
+	VkApplicationInfo appInfo{};
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = "Living NPC";
+	appInfo.applicationVersion = HTN_VERSION;
+	appInfo.pEngineName = "Living NPC Vulkan Engine";
+	appInfo.engineVersion = HTN_VERSION;
+	appInfo.apiVersion = VK_API_VERSION_1_0;
+
+	VkInstanceCreateInfo createInfo{};
+	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	createInfo.pApplicationInfo = &appInfo;
+
+	auto extensions = getRequiredExtensions();
+	createInfo.enabledExtensionCount = static_cast<u32>(extensions.size());
+	createInfo.ppEnabledExtensionNames = extensions.data();
+
+	VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
+	if (HTN_DEBUG) {
+		createInfo.enabledLayerCount = static_cast<u32>(validationLayers.size());
+		createInfo.ppEnabledLayerNames = validationLayers.data();
+
+		populateDebugMessengerCreateInfo(debugCreateInfo);
+		createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*)&debugCreateInfo;
+	}
+	else {
+		createInfo.enabledLayerCount = 0;
+		createInfo.pNext = nullptr;
+	}
+
+	if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+		throw std::runtime_error("Failed to create instance");
+	}
+}
+
+bool HTN::Device::checkValidationLayerSupport() {
+	u32 layerCount;
+	vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+	std::vector<VkLayerProperties> availableLayers(layerCount);
+	vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+	for (const char* layerName : validationLayers) {
+		bool layerFound = false;
+
+		for (const auto& layerProperty : availableLayers) {
+			if (strcmp(layerName, layerProperty.layerName) == 0) {
+				layerFound = true;
+				break;
+			}
+		}
+
+		if (!layerFound) return false;
+	}
+
+	return true;
+}
+
+std::vector<const char*> HTN::Device::getRequiredExtensions() {
+	u32 glfwExtensionCount = 0;
+	const char** glfwExtensions;
+
+	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+	std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+	if (HTN_DEBUG) {
+		extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+	}
+
+	return extensions;
+}
+
+void HTN::Device::populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo) {
+	createInfo = {};
+	createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+
+	createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+		VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+	createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+		VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+		VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+	createInfo.pfnUserCallback = debugCallback;
+}
+
+void HTN::Device::setupDebugMessenger() {
+	if (!HTN_DEBUG) return;
+
+	VkDebugUtilsMessengerCreateInfoEXT createInfo;
+	populateDebugMessengerCreateInfo(createInfo);
+
+	if (CreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS) {
+		throw std::runtime_error("Failed to setup debug messenger");
+	}
+}
+
+void HTN::Device::createSurface() {
+	if (glfwCreateWindowSurface(instance, window.getWindow(), nullptr, &surface) != VK_SUCCESS) {
+		throw std::runtime_error("Failed to create window surface");
+	}
+}
--- src/RenderingEngine/device.hpp
+++ src/RenderingEngine/device.hpp
@@ -1,10 +1,15 @@
 #pragma once
 #include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include "../defines.hpp"
 #include "Platform/window.hpp"
 
+#include <stdexcept>
+#include <iostream>
 #include <vector>
+#include <cstring>
 
 namespace HTN {
 
@@ -19,6 +24,7 @@ namespace HTN {
 		Device(const Device&) = delete;
 		Device& operator=(const Device&) = delete;
 
+		// Getter functions
 		VkInstance getInstance() const { return instance; }
 
 	private:
@@ -26,8 +32,14 @@ namespace HTN {
 
 		VkInstance instance = VK_NULL_HANDLE;
 		VkDebugUtilsMessengerEXT debugMessenger = VK_NULL_HANDLE;
+		VkSurfaceKHR surface = VK_NULL_HANDLE;
 
 		void createInstance();
+		bool checkValidationLayerSupport();
+		std::vector<const char*> getRequiredExtensions();
+		void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
+		void setupDebugMessenger();
+		void createSurface();
 	};
 
 } // namespace HTN
