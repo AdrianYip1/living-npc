5f275a08498dec246bcf46d81b1f4e468a9b0319
Adrian Yip <yipadrian1119@gmail.com>
2026-09-19 01:50:36 -0400

    feat: filled out the pipeline class with a single graphics pipeline, helper functions for rea r reading shaders too (which will be added later)

--- src/RenderingEngine/pipeline.hpp
+++ src/RenderingEngine/pipeline.hpp
@@ -4,15 +4,39 @@
 #include "../defines.hpp"
 #include "device.hpp"
 
+#include <fstream>
+#include <array>
+#include <string>
+
 namespace HTN {
 	class Pipeline {
 	public:
-		Pipeline(Device& _device);
+
+		// TODO: will probably need mutiple pipeline ctors for different things like rendering
+		// the scene, hdri (sky?), npcs, etc
+		Pipeline(Device& _device, const std::string& vertPath, const std::string& fragPath);
 		~Pipeline();
 		Pipeline(const Pipeline&) = delete;
 		Pipeline& operator=(const Pipeline&) = delete;
 
+		// getters
+		VkPipelineLayout getPipelineLayout() { return pipelineLayout; }
+		VkPipeline getGraphicsPipeline() { return graphicsPipeline; }
+		VkRenderPass getRenderpass() { return renderpass; }
+
 	private:
 		Device& device;
+		std::string vertPath;
+		std::string fragPath;
+
+		VkPipelineLayout pipelineLayout;
+		VkRenderPass renderpass;
+		VkPipeline graphicsPipeline;
+
+		void createRenderPass();
+		void createGraphicsPipeline();
+
+		static std::vector<char> readFile(const std::string& filename);
+		VkShaderModule createShaderModule(const std::vector<char>& shaderCode);
 	};
 } // namespace HTN
